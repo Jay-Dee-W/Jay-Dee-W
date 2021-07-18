1e4e714ea5a1ec07c36c8c786fd8a63a792d3847
Jay-Dee-W <jaydee.wilson@gmail.com>
2021-07-18 15:35:29 +0200

    commit

--- src/components/pages/Bio.jsx
+++ src/components/pages/Bio.jsx
@@ -1,37 +1,28 @@
 import './Pages.css'
 
 export default function Bio() {
-  
+
 
     return (
         <div className="Page">
             <h1>Welcome, I'm JD Wilson </h1>
             <h4>Johannesburg, South Africa.</h4>
+            <br />
+            <p>
+                I have over 20 years of experience in the IT field starting back in 1996 as a mainframe Change Controller with South African Airways (SAA) and many years as an MS SQL database Administrator for a number of different companies ending as an Enterprise Domain/Server Administrator with SITA in 2016. <br />
                 <br />
-                <p>
-                I have over 20 years’ experience in the IT field starting back in 1996
-                as a mainframe Change Controller with South African Airways (SAA) 
-                and many years as MS SQL database Administrator for a number of different companies 
-                ending as a Enterprise Domain/Server Administrator with SITA in 2016. <br />
-                <br />
-                After which I co-founded a web development and digital marketing consultancy, 
-                and re-skilled myself as a web developer creating and maintaining web-enabled solutions for a small number of private clients. 
-                In 2020 I upgrade my skill-set be completing an online boot-camp where I learnt JavaScript, HTML & CSS
-                and the MERN (Mongodb, Express, React, Nodejs) Full Stack. 
-                This also included exposure to git/github, Firebase/Firestore, Netlify and a number of related technologies<br />
+                After which I co-founded a web development and digital marketing consultancy, and re-skilled myself as a web developer creating and maintaining web-enabled solutions for a small number of private clients. In 2020 I upgrade my skill-set by completing an online boot camp where I learned JavaScript, HTML & CSS, and the MERN (MongoDB, Express, React, Nodejs) Full-Stack. This also included exposure to git/GitHub, Firebase/Firestore, Netlify, and a number of related technologies<br />
                 <br />
             </p>
             <p>
-                I'm passionate about creating innovative, customised solutions for my clients, that harness the best technologies and methodologies 
-                to meet and exceed their expectations. I understand that web sites and the solutions I offer are critical to my client business success
-                and that this requires constant open an honest communication between us.<br />
+                I'm passionate about creating innovative, customized solutions for my clients, that harness the best technologies and methodologies to meet and exceed their expectations. I understand that websites and the solutions I offer are critical to my client's business success and that this requires constant open and honest communication between us.<br />
                 <br />
                 If you would like to engage me for your web-enabled business solutions, please use the Contact link in the Side Navigation bar<br />
                 <br />
             </p>
             <p>
-                For more details on my work experience please select the Curriculum Vita link  or for examples of my work please select the Portfolio link<br />
-                <br/>
+                For more details on my work experience please select the Curriculum Vita link or for examples of my work please select the Portfolio link<br />
+                <br />
             </p>
         </div>
     )
--- src/components/pages/CV.jsx
+++ src/components/pages/CV.jsx
@@ -39,7 +39,7 @@ export default function CV() {
                 Oversee and implement the following phases in the product lifecycle: design, development, deployment, optimization, maintenance and support for Enterprise SQL database solutions.<br />
                 <br />
                 Deploying and customizing a CMDB SharePoint based site utilizing InfoPath Forms and WMI scripts to enable the creation of an effective Incident and Problem Management service within the Service Desk environment.<br />
-                Deploying and customizing a Service Desk SharePoint based site for international client
+                Deploying and customizing a Service Desk SharePoint based site for an international client
             </p>
             <br />
             <h3>SAA/Atraxis</h3>
@@ -47,9 +47,8 @@ export default function CV() {
             <h5>1996 - 2002</h5>
             <p>IMS / DB2 / ObjectStar and MS SQL Database Administration<br />
                 <br />
-                Ensure the correct scheduling of changes through-out the organization utilizing the mainframe-based utility InfoMan<br />
+                Ensure the correct scheduling and distribution of changes throughout the organization utilizing the mainframe-based utility InfoMan<br />
                 Coding the change flow procedures within InfoMan <br />
-                Distribution of information on changes to be made through-out the organization.
             </p>
             <br />
             <h1> Education </h1>
@@ -57,14 +56,191 @@ export default function CV() {
             <h3>Online BootCamp</h3>
             <h5>2020 - 2021</h5>
             <p> JS / HTML / CSS and MERN Full Stack Web Development </p>
+            <table>
+                <tr>
+                    <th>
+                        Certification
+                    </th>
+                    <th>
+                        Data issued
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="https://freecodecamp.org/certification/jaydeew/javascript-algorithms-and-data-structures" target='_blank'  rel="noreferrer">
+                            JavaScript Algorithms and Data Structures
+                        </a>
+                    </td>
+                    <td>
+                        <p>November 2020</p>
+                    </td>
+                </tr>
+            </table>
             <br />
-            <h3>CompTIA</h3>
-            <h5>2009 - 2012</h5>
-            <p> A+ Technician, Computer Architecture and ICT Technician </p>
+            <h3><a href="https://www.certmetrics.com/comptia/public/transcript.aspx?transcript=K1KJ8HZ2KBQQ1R37" target='_blank'  rel="noreferrer">CompTIA</a></h3>
+            <h5>2009</h5>
+            <p> CompTIA A+ (IT Technician) </p>
             <br />
-            <h3>MicroSoft</h3>
+            <h3><a href='https://mcp.microsoft.com/anonymous/transcript/validate' target='_blank' rel="noreferrer">MicroSoft</a></h3>
+            <p>Transcript ID: 845478 Access Code : Wilsonjd1 </p>
             <h5>2008 - 2012</h5>
-            <p> MCITP, Windows Server & MS SQL + Microsoft Certified Trainer</p>
+            <table>
+                <tr>
+                    <th>Active Certifications</th>	<th>Achievement Date</th>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft Certified Professional: Microsoft Certified Professional<br />
+                        Certification Number: E910-0079
+                    </td>
+                    <td>
+                        April 16, 2012
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Solutions Associate: Windows Server 2008<br />
+                        Certification Number: D717-2160
+                    </td>
+                    <td>
+                        April 16, 2012
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified IT Professional: Enterprise Administrator on Windows Server 2008<br />
+                        Certification Number: C070-1668
+                    </td>
+                    <td>
+                        August 31, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Technology Specialist: Windows Server 2008 Applications Infrastructure, Configuration<br />
+                        Certification Number: C070-1667
+                    </td>
+                    <td>
+                        July 24, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Technology Specialist: Windows Server 2008 Network Infrastructure, Configuration<br />
+                        Certification Number: C070-1666
+                    </td>
+                    <td>
+                        July 1, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Technology Specialist: Windows Server 2008 Active Directory, Configuration<br />
+                        Certification Number: C070-1665
+                    </td>
+                    <td>
+                        June 3, 2009
+                    </td>
+                </tr>
+                <tr>
+
+                    <th>Microsoft Certified Trainer History</th>	<th>Active From</th> <th> 	To</th>
+
+                </tr>
+                <tr>
+                    <td>
+                        MCT History
+                    </td>
+                    <td>
+                        December 7, 2009
+                    </td>
+                    <td>
+                        May 31, 2011
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Exams
+                    </th>
+                    <th>
+                        Date Completed
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        MD-100	Windows 10
+                    </td>
+                    <td>
+                        February 25, 2020
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        624	TS: Deploying and Maintaining Windows Vista Client and 2007 Microsoft Office System Desktops
+                    </td>
+                    <td>
+                        August 31, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        643	TS: Windows Server 2008 Applications Infrastructure, Configuring
+                    </td>
+                    <td>
+                        July 24, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        642	Windows Server 2008 Network Infrastructure, Configuring
+                    </td>
+                    <td>
+                        July 1, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        640	Windows Server 2008 Active Directory, Configuring
+                    </td>
+                    <td>
+                        June 3, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        647	Pro: Windows Server 2008, Enterprise Administrator
+                    </td>
+                    <td>
+                        April 29, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        431	TS: Microsoft SQL Server™ 2005 - Implementation and Maintenance
+                    </td>
+                    <td>
+                        November 11, 2008
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Technology Specialist: Business Desktop Deployment<br />
+                        Certification Number: C070-1648
+                    </td>
+                    <td>
+                        August 31, 2009
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Microsoft® Certified Technology Specialist: SQL Server 2005<br />
+                        Certification Number: C070-1647
+                    </td>
+                    <td>
+                        November 11, 2008
+                    </td>
+                </tr>
+            </table>
             <br />
         </div>
     )
